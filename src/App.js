059d2527045fd1d081e474ed5bae7601165ad10f
lmedhub <lucas_hubner1@hotmail.com>
2022-05-01 23:04:40 -0300

    HTML pronto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FormBusca from './components/Options';
+import Titulo from './components/Titulo';
+
+function App() {
+  return (
+    <div className="App">
+      <Titulo/>
+      <FormBusca/>
+    </div>
+  );
+}
+
+export default App;
