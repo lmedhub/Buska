c56d8cbd6367b3d17d631f542270bd9c9d86d3b3
lmedhub <lucas_hubner1@hotmail.com>
2022-05-03 02:19:07 -0300

    MPV

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,68 +1,86 @@
 import React from "react";
-
-
+import { sites } from "./checkBoxes/CheckBoxes";
 
 
 export default function FormBusca() {
 
+    const [redditIsChecked, setReddIsChecked] = React.useState(false)
+    const [stackIsChecked, setStackIsChecked] = React.useState(false)
+    const [YoutIsChecked, setYoutIsChecked] = React.useState(false)
+    const [site, setSite] = React.useState("")
 
+    const [userValue, setUserValue] = React.useState()
 
+    function handleChangeR() {
+        setReddIsChecked(!redditIsChecked)
+        setSite("+site%3Areddit.com/")
+    }
+    function handleChangeS() {
+        setStackIsChecked(!stackIsChecked)
+        setSite("+site%3Astackoverflow.com/")
+    }
+    function handleChangeY() {
+        setYoutIsChecked(!YoutIsChecked)
+        setSite("+site%3Ayoutube.com/")
+    }
+    
+    function getInputValue(event) {
+        setUserValue(event.target.value)
+    }
+    function handleSubmit(event) {
+        event.preventDefault()
+        window.open("http://google.com/" + "search?q=" + userValue + site)
+      }
+
+    
     return (
         <div className="Form">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="checkSites">    
                     <p>Sites para pesquisar</p>
                     <label>
+                        <input defaultChecked
+                        type="radio"
+                        name="site"
+                        id="radio1"
+                        onChange={() => setSite("")}
+                        ></input>
+                        Qualquer
+                    </label>
+                    <label>
                         <input 
-                        type="checkbox"
-                        name="reddit"
+                        type="radio"
+                        name="site"
+                        id="radio2"
+                        onChange={handleChangeR}
                         ></input>
                         Reddit
                     </label>
                     <label>
                         <input 
-                        type="checkbox"
-                        name="stack"
+                        type="radio"
+                        name="site"
+                        id="radio3"
+                        onChange={handleChangeS}
                         ></input>
                         StackOverFlow
                     </label>
                     <label>
                         <input 
-                        type="checkbox"
-                        name="youtube"
+                        type="radio"
+                        name="site"
+                        onChange={handleChangeY}
                         ></input>
                         Youtube
                     </label>
                 </div>
                 <p>Período de tempo</p>
-                <div className="checkData">
-
-                    <label>
-                        <input
-                        type="checkbox"
-                        name="tempo"
-                        ></input>
-                        Ultimo ano
-                    </label>
-                    <label>
-                        <input
-                        type="checkbox"
-                        name="tempo"
-                        ></input>
-                        Ultimos 6 meses
-                    </label>
-                    <label>
-                        <input
-                        type="checkbox"
-                        name="tempo"
-                        ></input>
-                        Ultimo mês
-                    </label>
-                </div>
+                
                 <div className="caixaPesquisa">
                     <input autoFocus
                         type="text"
                         name="caixa"
+                        onChange={getInputValue}
                     ></input>
                     <input
                         type="submit"
---
+++ src/components/checkBoxes/CheckBoxes.js
@@ -0,0 +1,17 @@
+export const sites = [
+    {
+        name:"Reddit",
+        key:"checkbox1",
+        label:"Reddit",
+    },
+    {
+        name:"StackOverFlow",
+        key:"checkbox2",
+        label:"StackOverFlow",
+    },
+    {
+        name:"Youtube",
+        key:"checkbox3",
+        label:"Youtube",
+    }
+]
